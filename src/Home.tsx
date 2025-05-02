94e89845da55658ec9bb8bd0301b674450279333
Lucas Gomes <lulinux2109@gmail.com>
2025-05-01 21:48:44 -0300

    :sparkles: feat: create homePage

---
+++ src/Home.tsx
@@ -0,0 +1,20 @@
+import { SearchBar } from "./SearchBar/SearchBar"
+
+export const HomePage = () => {
+  return (
+    <>
+      <SearchBar onSearch={(query) => {
+        dispatch({ type: "SET_SEARCH_QUERY", payload: query })
+      }} />
+
+      {/* <div className="books-grid"
+        {filteredBoook.lenght > 0 ? (
+          filteredBooks.map((book) => {
+
+          }))
+        )}
+      </div> */}
+    </>
+  )
+
+}
