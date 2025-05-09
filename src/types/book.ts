a536713e97168dbb0bf6bdf6c8aa0c1bd2bcef1b
Lucas Gomes <lulinux2109@gmail.com>
2025-05-08 22:14:00 -0300

    feat: creates the book interface

---
+++ src/types/book.ts
@@ -0,0 +1,11 @@
+export interface Book {
+  id: string
+  title: string
+  author: string
+  category: string
+  isbn: string
+  publicationYear: number
+  status: "available" | "borrowed"
+}
+
+export type BookFormData = Omit<Book, "id" | "status">
